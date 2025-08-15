5a62a21f92be472a31ac767c62d10367287c779d
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2025-08-15 19:45:49 -0300

    Initial Commit

---
+++ TheOffice/TheOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheOfficeApp.swift
+//  TheOffice
+//
+//  Created by Adriano Rodrigues Vieira on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TheOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheOfficeTests/TheOfficeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheOfficeTests.swift
+//  TheOfficeTests
+//
+//  Created by Adriano Rodrigues Vieira on 15/08/25.
+//
+
+import Testing
+@testable import TheOffice
+
+struct TheOfficeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
